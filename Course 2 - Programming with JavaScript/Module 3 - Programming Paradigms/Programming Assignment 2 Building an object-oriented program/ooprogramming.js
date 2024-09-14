5bc9efbae3eb797113d5261df56cdc020ebe4126
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2024-09-14 16:32:38 +0500

    Updated

---
+++ Course 2 - Programming with JavaScript/Module 3 - Programming Paradigms/Programming Assignment 2 Building an object-oriented program/ooprogramming.js
@@ -0,0 +1,48 @@
+// Task 1: Code a Person class
+class Person {
+    name = 'Tom';
+    age = 20;
+    energy = 100;
+
+    constructor(name, age, energy) {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+    }
+
+    sleep = () => {
+        this.energy += 10;
+    };
+
+    doSomethingFun = () => {
+        this.energy -= 10;
+    };
+}
+// Task 2: Code a Worker class inherits Person
+class Worker extends Person {
+    xp = 0;
+    hourlyWage = 10;
+
+    constructor(name, age, energy, xp, hourlyWage) {
+        super(name, age, energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+
+    goToWork = () => {
+        this.xp += 10;
+    }
+}
+// Task 3: Code an intern object, run methods
+function intern() {
+    const intern = new Worker('Bob', 21, 110, 0, 10);
+    intern.goToWork();
+    return intern;
+    }
+
+// Task 4: Code a manager object, methods
+function manager() {
+    const manager = new Worker('Alice', 30, 120, 100, 30);
+    manager.doSomethingFun();
+    return manager;
+}
