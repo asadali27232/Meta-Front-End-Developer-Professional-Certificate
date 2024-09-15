e6882a4d303e0f70f4cfc7bbe77bd68df4ad6678
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2024-09-15 12:47:18 +0500

    Updated

--- Course 2 - Programming with JavaScript/Module 3 - Programming Paradigms/Programming Assignment 3 Array and object iteration/solution.js
+++ Course 2 - Programming with JavaScript/Module 3 - Programming Paradigms/Programming Assignment 3 Array and object iteration/solution.js
@@ -1,6 +1,45 @@
 // Task 1
+var dairy = [
+    'cheese',
+    'sour cream',
+    'milk',
+    'yogurt',
+    'ice cream',
+    'milkshake',
+];
+
+function logDairy() {
+    for (dairyItem of dairy) {
+        console.log(dairyItem);
+    }
+}
+
+logDairy();
 
 // Task 2
+const animal = {
+    canJump: true,
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (key of Object.keys(bird)) {
+        console.log(key + ': ' + bird[key]);
+    }
+}
+
+birdCan();
 
 // Task 3
+function animalCan() {
+    for (let key in bird) {
+        console.log(key + ': ' + bird[key]);
+    }
+}
 
+animalCan();
